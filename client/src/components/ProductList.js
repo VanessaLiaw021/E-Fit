ab0ee6a60ada34f8b104299c77133f9a9daeedd8
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-31 17:40:39 -0400

    started desginging the productlist page

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className="main">
+      <h2 className="headings">Products</h2>
+      
+      <div className="category">
+        <ul>  
+          <li>All</li>
+          <li>Women's Apparel</li>
+          <li>Men's Apparel</li>
+          <li>Equipment</li>
+          <li>Accessories</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
