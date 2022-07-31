0ba61b26c256c799561ff35417174bd4d7f99a50
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-31 18:32:35 -0400

    fix some changes in productlist.js, category name is in the seeded data file

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,22 +1,18 @@
+//Import required packages
 import React from 'react';
-import styled from 'styled-components';
 
+//ProductList Component
 const ProductList = () => {
   return (
     <div className="main">
       <h2 className="headings">Products</h2>
       
       <div className="category">
-        <ul>  
-          <li>All</li>
-          <li>Women's Apparel</li>
-          <li>Men's Apparel</li>
-          <li>Equipment</li>
-          <li>Accessories</li>
-        </ul>
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
+//Export ProductList
 export default ProductList;
