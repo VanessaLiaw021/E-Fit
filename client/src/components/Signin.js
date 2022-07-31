cbc0dc868bd13cc3855347623dd83ac390052a5a
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-31 15:32:30 -0400

    created the form for signin component in Signin.js

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,8 +1,50 @@
-import React from 'react'
+//Import required packages and files
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Signin = () => {
+
+  //Styled Component for Form
+  const SignInForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 30%;
+    margin: 140px auto 0 auto; 
+    border: 1px solid black;
+  `;
+
+  //Styled Components for Button
+  const Button = styled.button`
+    background-color: #71a6d2;
+    border: none;
+    border-top-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    padding: 5px 0;
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2);
+    width: 80px;
+    color: white;
+    font-size: 18px;
+    margin: 5px 0 20px 0;
+    cursor: pointer;
+  `;
+
   return (
-    <div>Signin</div>
+    <SignInForm>
+      <h2>Sign Up</h2>
+      <div className="form-group bottom-form">
+        <label>Email:</label>
+        <input type="text" placeholder="Email"/>
+      </div>
+      <div className="form-group bottom-form">
+        <label>Password:</label>
+        <input type="password" placeholder="Password"/>
+      </div>
+      <p id="account">Don't have an account? <Link to="/signup" className="direct-signin">Sign Up</Link></p>
+      <Button>Sign In</Button>
+    </SignInForm>
   )
 }
 
