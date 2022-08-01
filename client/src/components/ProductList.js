dbc8d0428ca59797cd334453d139e33bc2ec24b4
GrahamP98 <g.purnell15@gmail.com>
2022-08-01 13:15:22 -0400

    fixed imports in ProductList

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,11 +1,11 @@
 //Import required packages
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import ProductItem from './ProductItem';
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
 import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import { QUERY_PRODUCTS } from '../utils/queries';
+import { idbPromise } from '../utils/helpers';
 import Footer from './Footer';
 
 //ProductList Component
