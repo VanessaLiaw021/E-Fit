74c0ae8800e7d6701fb27ad286f803bd6189bba7
VanessaLiaw021 <vliaw021@gmail.com>
2022-08-01 10:27:59 -0400

    fixing navbar styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
   //Styled Component for the Header
   const Header = styled.header`
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
-    padding: 25px 0;
+    padding: 25px 30px;
     background-color: #ffe4e1;
   `;  
 
@@ -23,26 +23,26 @@ const Navbar = () => {
 
   //Styled Component for the NavBar List
   const NavList = styled.li`
-    margin: 0 20px;
+    margin: 0 18px;
     font-size: 20px;
     line-height: 30px;
   `;
 
   return (
     <Header>
-        <h1>E-Fit</h1>
+        <Link to="/"><h1>E-Fit</h1></Link>
 
         {/* NavBar */}
         <nav>
           <Navbar>
             <NavList>
-              <Link to='/' className="links nav-link">Home</Link>
+              <Link to='/' className="links nav-link middle-nav">Home</Link>
             </NavList>
             <NavList>
-              <Link to='/exercises' className="links nav-link">Exercises</Link>
+              <Link to='/exercises' className="links nav-link middle-nav">Exercises</Link>
             </NavList>
             <NavList>
-              <Link to='/products' className="links nav-link">Products</Link>
+              <Link to='/products' className="links nav-link middle-nav">Products</Link>
             </NavList>
             <NavList>
               <Link to='/signin' className="links nav-link">Sign In</Link>
